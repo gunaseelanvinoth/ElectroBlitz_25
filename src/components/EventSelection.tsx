c03d59a25abc68fa21eb90f9cb9b49c3c7cb72c8
Gunaseelan V <150247119+gunaseelanvinoth@users.noreply.github.com>
2025-10-13 16:12:57 +0530

    Update EventSelection.tsx

--- src/components/EventSelection.tsx
+++ src/components/EventSelection.tsx
@@ -245,7 +245,7 @@ const EventSelection: React.FC<EventSelectionProps> = ({
         // Workshops
         { id: 'frontend', title: 'Frontend', description: 'Venue: VLSI Lab.',  icon: '🎨', category: 'workshop' },
         { id: 'pcb-assembling', title: 'PC Assembling', description: 'Venue: Microwave Lab.', icon: '🛠️', category: 'workshop' },
-        { id: 'eda-tools', title: 'EDA Tools', description: 'Venue: 3rd Floor Lab.',  icon: '🧩', category: 'workshop' }
+       /* { id: 'eda-tools', title: 'EDA Tools', description: 'Venue: 3rd Floor Lab.',  icon: '🧩', category: 'workshop' }*/
     ];
 
     useEffect(() => {
